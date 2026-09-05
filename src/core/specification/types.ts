842aae53a069ccc2d881ebda06e87f61100addf3
kameusagiyahoo <nywhatisbudou@yahoo.co.jp>
2026-09-06 05:50:34 +0900

    feat: add specification document types

---
+++ src/core/specification/types.ts
@@ -0,0 +1,30 @@
+import type { NodeKind } from '../graph/types';
+
+export type SpecificationNode = {
+  id: string;
+  kind: NodeKind;
+  name: string;
+  description: string;
+  configLines: string[];
+  incomingNodeIds: string[];
+  outgoingNodeIds: string[];
+};
+
+export type SpecificationConnection = {
+  id: string;
+  sourceNodeId: string;
+  targetNodeId: string;
+  sourceName: string;
+  targetName: string;
+};
+
+export type SpecificationDocument = {
+  projectId: string;
+  projectName: string;
+  nodes: SpecificationNode[];
+  connections: SpecificationConnection[];
+  entryNodeIds: string[];
+  exitNodeIds: string[];
+  notes: string[];
+  markdown: string;
+};
